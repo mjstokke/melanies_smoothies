de2a745d47b1ee82b31a63e63edc89b61f29757e
mjstokke <michael.stokke@sanfordhealth.org>
2026-09-23 16:47:05 -0500

    Add pandas import to streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import os
 from snowflake.snowpark.functions import col
 import requests
+import pandas
 
 # Write directly to the app
 st.title(f"🥤 Customize Your Smoothie! 🥤")
